e54a35b718f079512c0ef6c121b8bcd3803ee293
amazing-hashmap-2071 <haim11293@aol.com>
2019-05-05 15:22:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,6 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 var superheroines = ["catwoman", "she-hulk", "mystique"];
 superheroines.push("wonder woman");
+
+var cities = ["New York", "San Fransisco"];
+cities.unshift("Philadelphia");
